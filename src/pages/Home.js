1709f2ef225a7e1421a334ef4675bae19f2c8284
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-05-06 13:51:08 +0530

    Toaster Config

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,52 +1,83 @@
 import React, { useState } from "react";
-import { v4 as uuidV4 } from 'uuid';
+import { v4 as uuidV4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-
-  const [roomId, setRoomId] = useState('');
-  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
 
   const createNewRoom = (e) => {
     e.preventDefault();
     const id = uuidV4();
     setRoomId(id);
-    // toast.success('Created a new room');
-};
+    toast.success("Created a new room");
+  };
 
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error("ROOM ID & username is required");
+      return;
+    }
+
+    // Redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) => {
+    if (e.code === 'Enter') {
+        joinRoom();
+    }
+};
 
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
-        <img className="homePageLogo" src="/code-sync.png" alt="code-sync-logo" />
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
         <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
         <div className="inputGroup">
-          <input 
-          type="text" 
-          className="inputBox" 
-          placeholder="ROOM ID" 
-          onChange={(e) => setRoomId(e.target.value)}
-          value={roomId} />
-          <input 
-          type="text" 
-          className="inputBox" 
-          placeholder="USERNAME" 
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}/>
-          <button className="btn joinBtn">Join</button>
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+            onKeyUp={handleInputEnter}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            onKeyUp={handleInputEnter}
+          />
+          <button className="btn joinBtn" onClick={joinRoom}>
+            Join
+          </button>
           <span className="createInfo">
-          If you don't have an invite then create &nbsp;
-          <a onClick={createNewRoom}
-          href="" 
-          className="createNewBtn">new room</a>
+            If you don't have an invite then create &nbsp;
+            <a onClick={createNewRoom} href="" className="createNewBtn">
+              new room
+            </a>
           </span>
         </div>
       </div>
       <footer>
-                <h4>
-                    Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/swarupcs">Swarup Das</a>
-                </h4>
-            </footer>
+        <h4>
+          Built with 💛 &nbsp; by &nbsp;
+          <a href="https://github.com/swarupcs">Swarup Das</a>
+        </h4>
+      </footer>
     </div>
   );
 };
