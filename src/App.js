8b53f5bba4743f0d9c01678d9a91660111aac1b4
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-05-06 13:13:09 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import EditorPage from './pages/EditorPage';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/editor/:roomId' element={<EditorPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      Home Page
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img className="homePageLogo" src="/code-sync.png" alt="code-sync-logo" />
+        <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input type="text" className="inputBox" placeholder="ROOM ID" />
+          <input type="text" className="inputBox" placeholder="USERNAME" />
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+          If you don't have an invite then create &nbsp;
+          <a href="" className="createNewBtn">new room</a>
+          </span>
+        </div>
+      </div>
+      <footer>
+                <h4>
+                    Built with 💛 &nbsp; by &nbsp;
+                    <a href="https://github.com/swarupcs">Swarup Das</a>
+                </h4>
+            </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
